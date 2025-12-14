586a3b92160defb89292c9434da05601d72892ef
Luca <luca@teapot.ovh>
2025-12-14 09:37:28 +0100

    ui: add teapot logo

--- lib/ui/components/logo.go
+++ lib/ui/components/logo.go
@@ -1,18 +1,22 @@
 package components
 
 import (
+	_ "embed"
+
 	g "maragu.dev/gomponents"
 	h "maragu.dev/gomponents/html"
 
 	"github.com/teapotovh/teapot/lib/ui"
 )
 
+//go:embed logo.svg
+var logoSVG []byte
+
 var LogoStyle = ui.MustParseStyle(`
 	height: var(--size-7);
 	width: var(--size-7);
-	background: var(--theme-brand0);
 `)
 
 func Logo(ctx ui.Context) g.Node {
-	return h.Div(ctx.Class(LogoStyle))
+	return h.Div(ctx.Class(LogoStyle), g.Raw(string(logoSVG)))
 }
