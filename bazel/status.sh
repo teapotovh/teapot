8fb1e7a07dd605763c2a6ff1a3ae520e96762870
Luca <luca@teapot.ovh>
2026-04-29 10:26:10 +0200

    bazel: proper image building

--- bazel/status.sh
+++ bazel/status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-VERSION=${IMAGE_TAG:-$(git describe --tags --abbrev=0 2>/dev/null || echo 'dev')}
+VERSION=${GITHUB_REF_NAME:-$(git describe --tags --abbrev=0 2>/dev/null || echo 'dev')}
 echo "STABLE_TEAPOT_VERSION $VERSION"
