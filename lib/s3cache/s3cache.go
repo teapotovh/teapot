a4222dba493897d873acbe2e99a81d03c6c74b03
Luca <luca@teapot.ovh>
2026-07-16 00:02:37 +0200

    calendar: s3 debug log

--- lib/s3cache/s3cache.go
+++ lib/s3cache/s3cache.go
@@ -189,6 +189,7 @@ func (c *S3Cache) readCached(key string, expected Hash) ([]byte, bool, error) {
 }
 
 func (c *S3Cache) fetchAndStore(ctx context.Context, key string) (data []byte, hash Hash, err error) {
+	c.logger.DebugContext(ctx, "fetching from remote S3 store", "key", key)
 	obj, err := c.client.GetObject(ctx, c.bucket, key, minio.GetObjectOptions{})
 	if err != nil {
 		return nil, ZeroHash, fmt.Errorf("error while getting object %q: %w", key, err)
